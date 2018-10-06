60b138a4694e8cc813a8552756fe63c0f8d1e548
Paolo <p.degrazia@gmail.com>
2018-10-06 01:41:31 +0100

    [MockRequest] added happy path test for RequestClass; renamed mock test method

--- tests/MockRequestTest.py
+++ tests/MockRequestTest.py
@@ -4,8 +4,16 @@ from mymodule import RequestClass, MainClass
 
 
 class MockRequestTest(unittest.TestCase):
+    def test_post_request(self):
+        response = RequestClass.post_request()
+
+        self.assertTrue(response['title'] == 'foo')
+        self.assertTrue(response['id'] == 101)
+        self.assertTrue(response['body'] == 'bar')
+        self.assertTrue(response['userId'] == 1)
+
     @patch('mymodule.RequestClass')
-    def test_post_request(self, MockRequestClass):
+    def test_mock_post_request(self, MockRequestClass):
         mock_request = MockRequestClass()
 
         mock_request.post_request.return_value = {'access_token': 'some_access_token'}
@@ -21,8 +29,9 @@ class MainClassTest(unittest.TestCase):
     @patch('mymodule.RequestClass')
     def test_request(self, mock_request):
         mock_request.post_request.return_value = {'access_token': 'some_access_token'}
-g
+
         mc = MainClass()
         call = mc.make_call()
 
         self.assertTrue('access_token' in call.keys())
+        self.assertTrue(call['access_token'] == 'some_access_token')
